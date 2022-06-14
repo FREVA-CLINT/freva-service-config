04120eccfe5905da91502ad48917932f4bffaff5
k204230 <bergemann@dkrz.de>
2022-06-14 18:30:01 +0200

    Update create core script

--- solr/create_core.sh
+++ solr/create_core.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for core in  $CORE latest;do
-    if [ ! -d "/var/solr/data/$core" ];then
-        precreate-core $core
-        cp /opt/solr/managed-schema.xml /var/solr/data/$core/conf/
+    if [ ! -d "/var/solr/data/$this_core" ];then
+        precreate-core $this_core
+        cp /opt/solr/managed-schema.xml /var/solr/data/$this_core/conf/
     fi
 done

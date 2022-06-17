4c05aa28f6cb41beaaf6d25359ae0f029b501d01
k204230 <bergemann@dkrz.de>
2022-06-17 16:05:19 +0200

    Add django tables to template (I don't know why this is not working without)

--- mysql/create_tables.sql
+++ mysql/create_tables.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.7.4-MariaDB, for Linux (x86_64)
+-- MariaDB dump 10.19  Distrib 10.8.3-MariaDB, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: freva
+-- Host: 127.0.0.1    Database: regiklimdb
 -- ------------------------------------------------------
--- Server version	8.0.29
+-- Server version	10.8.3-MariaDB-1:10.8.3+maria~jammy
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -22,11 +22,11 @@
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_group` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(150) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=109 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,14 +36,14 @@ CREATE TABLE `auth_group` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_group_permissions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `group_id` int NOT NULL,
-  `permission_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `group_id` (`group_id`,`permission_id`),
   KEY `auth_group_permissions_5f412f9a` (`group_id`),
   KEY `auth_group_permissions_83d7f98b` (`permission_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -53,14 +53,14 @@ CREATE TABLE `auth_group_permissions` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_permission` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `content_type_id` int NOT NULL,
+  `content_type_id` int(11) NOT NULL,
   `codename` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `content_type_id` (`content_type_id`,`codename`),
   KEY `auth_permission_37ef4eb4` (`content_type_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=80 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `auth_permission` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_user` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `password` varchar(128) NOT NULL,
   `last_login` datetime DEFAULT NULL,
   `is_superuser` tinyint(1) NOT NULL,
@@ -83,7 +83,7 @@ CREATE TABLE `auth_user` (
   `date_joined` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,14 +93,14 @@ CREATE TABLE `auth_user` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_user_groups` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `group_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`,`group_id`),
   KEY `auth_user_groups_6340c63c` (`user_id`),
   KEY `auth_user_groups_5f412f9a` (`group_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1209 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -110,14 +110,14 @@ CREATE TABLE `auth_user_groups` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_user_user_permissions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `permission_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`,`permission_id`),
   KEY `auth_user_user_permissions_6340c63c` (`user_id`),
   KEY `auth_user_user_permissions_83d7f98b` (`permission_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=317 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,18 +127,33 @@ CREATE TABLE `auth_user_user_permissions` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `django_admin_log` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `action_time` datetime NOT NULL,
-  `user_id` int NOT NULL,
-  `content_type_id` int DEFAULT NULL,
-  `object_id` longtext,
+  `user_id` int(11) NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `object_id` longtext DEFAULT NULL,
   `object_repr` varchar(200) NOT NULL,
-  `action_flag` smallint unsigned NOT NULL,
+  `action_flag` smallint(5) unsigned NOT NULL,
   `change_message` longtext NOT NULL,
   PRIMARY KEY (`id`),
   KEY `django_admin_log_6340c63c` (`user_id`),
   KEY `django_admin_log_37ef4eb4` (`content_type_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=530 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `django_content_type`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `django_content_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `app_label` (`app_label`,`model`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,7 +163,7 @@ CREATE TABLE `django_admin_log` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `django_flatpage` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(100) NOT NULL,
   `title` varchar(200) NOT NULL,
   `content` longtext NOT NULL,
@@ -157,7 +172,7 @@ CREATE TABLE `django_flatpage` (
   `registration_required` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `django_flatpage_c379dc61` (`url`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,14 +182,14 @@ CREATE TABLE `django_flatpage` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `django_flatpage_sites` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `flatpage_id` int NOT NULL,
-  `site_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `flatpage_id` int(11) NOT NULL,
+  `site_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `flatpage_id` (`flatpage_id`,`site_id`),
   KEY `django_flatpage_sites_872c4601` (`flatpage_id`),
   KEY `django_flatpage_sites_99732b5c` (`site_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,12 +199,12 @@ CREATE TABLE `django_flatpage_sites` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `django_migrations` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `app` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `applied` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -214,11 +229,12 @@ CREATE TABLE `django_session` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `django_site` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `domain` varchar(100) NOT NULL,
   `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `django_site_domain_a2e37b91_uniq` (`domain`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -228,7 +244,7 @@ CREATE TABLE `django_site` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `externaluser_externaluser` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` varchar(100) NOT NULL,
   `status_changed` datetime NOT NULL,
   `first_name` varchar(255) NOT NULL,
@@ -250,10 +266,10 @@ CREATE TABLE `externaluser_externaluser` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `hindcast_frontend_hindcastevaluation` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `score` varchar(50) NOT NULL,
-  `eva_time_start` int NOT NULL,
-  `eva_time_end` int NOT NULL,
+  `eva_time_start` int(11) NOT NULL,
+  `eva_time_end` int(11) NOT NULL,
   `region` varchar(255) NOT NULL,
   `time_frequency` varchar(255) NOT NULL,
   `variable` varchar(255) NOT NULL,
@@ -272,16 +288,16 @@ CREATE TABLE `hindcast_frontend_hindcastevaluation` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history_configuration` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `history_id_id` int NOT NULL,
-  `parameter_id_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `history_id_id` int(11) NOT NULL,
+  `parameter_id_id` int(11) NOT NULL,
   `md5` varchar(32) NOT NULL,
-  `value` longtext,
+  `value` longtext DEFAULT NULL,
   `is_default` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `history_configuration_05e95c0f` (`history_id_id`),
   KEY `history_configuration_c3d9a846` (`parameter_id_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=37258 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -291,22 +307,22 @@ CREATE TABLE `history_configuration` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history_history` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` datetime NOT NULL,
   `tool` varchar(50) NOT NULL,
   `version` varchar(20) NOT NULL,
   `configuration` longtext NOT NULL,
   `slurm_output` longtext NOT NULL,
-  `host` longtext NOT NULL,
   `uid` varchar(30) NOT NULL,
-  `status` int NOT NULL,
-  `flag` int NOT NULL DEFAULT '0',
-  `version_details_id` int NOT NULL DEFAULT '1',
+  `status` int(11) NOT NULL,
+  `flag` int(11) NOT NULL DEFAULT 0,
+  `version_details_id` int(11) NOT NULL DEFAULT 1,
   `caption` varchar(255) DEFAULT NULL,
+  `host` longtext DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `history_history_82ae9392` (`uid`),
   KEY `version_details_id` (`version_details_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2111 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -316,15 +332,15 @@ CREATE TABLE `history_history` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history_historytag` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `history_id_id` int NOT NULL,
-  `type` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `history_id_id` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
   `text` longtext NOT NULL,
   `uid` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `history_historytag_05e95c0f` (`history_id_id`),
   KEY `history_historytag_82ae9392` (`uid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2130 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -334,14 +350,14 @@ CREATE TABLE `history_historytag` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history_result` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `history_id_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `history_id_id` int(11) NOT NULL,
   `output_file` longtext NOT NULL,
-  `file_type` int NOT NULL,
+  `file_type` int(11) NOT NULL,
   `preview_file` longtext NOT NULL,
   PRIMARY KEY (`id`),
   KEY `history_result_05e95c0f` (`history_id_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4189 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -351,9 +367,9 @@ CREATE TABLE `history_result` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history_resulttag` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `result_id_id` int NOT NULL,
-  `type` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `result_id_id` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
   `text` longtext NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -366,16 +382,16 @@ CREATE TABLE `history_resulttag` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `plugins_parameter` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `parameter_name` varchar(50) NOT NULL,
   `parameter_type` varchar(50) NOT NULL,
   `tool` varchar(50) NOT NULL,
   `version` varchar(20) NOT NULL,
   `mandatory` tinyint(1) NOT NULL,
   `default` varchar(255) DEFAULT NULL,
-  `impact` int NOT NULL,
+  `impact` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1844 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -385,12 +401,12 @@ CREATE TABLE `plugins_parameter` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `plugins_toolpullrequest` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime NOT NULL,
   `tool` varchar(50) NOT NULL,
   `tagged_version` varchar(50) NOT NULL,
   `status` varchar(10) NOT NULL,
-  `user_id` int NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -402,7 +418,7 @@ CREATE TABLE `plugins_toolpullrequest` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `plugins_version` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` datetime NOT NULL,
   `tool` varchar(50) NOT NULL,
   `version` varchar(20) NOT NULL,
@@ -410,7 +426,7 @@ CREATE TABLE `plugins_version` (
   `internal_version_api` varchar(40) NOT NULL,
   `repository` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=123 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -420,16 +436,16 @@ CREATE TABLE `plugins_version` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `solr_usercrawl` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime NOT NULL,
   `status` varchar(10) NOT NULL,
-  `user_id` int NOT NULL,
+  `user_id` int(11) NOT NULL,
   `path_to_crawl` varchar(1000) NOT NULL,
   `tar_file` varchar(255) NOT NULL,
   `ingest_msg` longtext NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id_refs_id_dc9f4a71` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -441,4 +457,4 @@ CREATE TABLE `solr_usercrawl` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-16 12:41:41
+-- Dump completed on 2022-06-17 16:01:59

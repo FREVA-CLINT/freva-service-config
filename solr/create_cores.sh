088c1a6c8a4e764c1340e3f786ddaa3802ccea70
antarcticrainforest <martin.bergemann@posteo.org>
2023-08-16 15:13:40 +0200

    Always update solr config

--- solr/create_cores.sh
+++ solr/create_cores.sh
@@ -3,8 +3,8 @@
 for this_core in  $CORE latest;do
     if [ ! -d "/var/solr/data/$this_core" ];then
         precreate-core $this_core
-        cp /opt/solr/managed-schema.xml /var/solr/data/$this_core/conf/
-        rm -rf /var/solr/data/$this_core/conf/synonyms.txt
-        ln -s /opt/solr/synonyms.txt /var/solr/data/$this_core/conf/synonyms.txt
     fi
+    cp /opt/solr/managed-schema.xml /var/solr/data/$this_core/conf/
+    rm -rf /var/solr/data/$this_core/conf/synonyms.txt
+    ln -s /opt/solr/synonyms.txt /var/solr/data/$this_core/conf/synonyms.txt
 done

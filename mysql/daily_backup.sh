233df76adddb7346f999f634961505b14c4ea2cd
antarcticrainforest <martin.bergemann@posteo.org>
2025-04-10 16:19:02 +0200

    mariadb -> mysql

--- mysql/daily_backup.sh
+++ mysql/daily_backup.sh
@@ -4,11 +4,11 @@
 mkdir -p ${BACKUP_DIR}
 backup_f=${BACKUP_DIR}/backup-$(date +%Y%m%d_%H%M%S).sql.gz
 n_to_keep=$(echo $NUM_BACKUPS|awk '{print $1-1}')
-files_to_keep=$(ls -t ${BACKUP_DIR}/backup-*.sql.gz |head -n ${n_to_keep})
+files_to_keep=$(ls -t ${BACKUP_DIR}/backup-*.sql.gz |head -n ${n_to_keep} 2> /dev/null)
 for file in $(ls ${BACKUP_DIR}/backup-*.sql.gz);do
     is_new_file=$(echo ${files_to_keep} |grep $file)
     if [ "x${is_new_file}" = "x" ];then
         rm $file
     fi
 done
-mariadb-dump -u root -h localhost -p"${MYSQL_ROOT_PASSWORD}" --all-databases | gzip -c -9 -q > $backup_f
+mysqldump -u root -h localhost -p"${MYSQL_ROOT_PASSWORD}" --all-databases| gzip -c -9 -q > $backup_f

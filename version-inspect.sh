f5ca55922eb4cb4b608e1645a4d1d5c8ee0cbf56
antarcticrainforest <martin.bergemann@posteo.org>
2025-04-14 09:29:20 +0200

    Write detected image verson to file.

--- version-inspect.sh
+++ version-inspect.sh
@@ -2,7 +2,7 @@
 #set -e pipefail
 order=(podman docker)
 path=""
-if [ -z "$1" ];then
+if [ -z "$1" ] || [ ! -f "$1/Dockerfile" ];then
     echo "Usage: $0 <image_name> >&2"
     exit 1
 fi
@@ -17,10 +17,14 @@ if [ -z "$path" ];then
     exit 1
 fi
 
+image=$(head -n1 $1/Dockerfile | awk '{print $NF}')
+image_version=$(echo $image | cut -d : -f2)
 
-$path pull docker.io/${1}:latest &> /dev/null
+$path pull $image
 
-if [ "$1" = "redis" ];then
+if [ "$image_version" != "latest" ];then
+    version=$image_version
+elif [ "$1" = "redis" ];then
     version=$($path run -it redis sh -c 'echo $REDIS_VERSION')
 else
     version=$($path inspect --format='{{ index .Config.Labels "org.opencontainers.image.version" }}' $1)
@@ -30,4 +34,5 @@ if [ -z "$version" ];then
     echo "Error: could not find version for $1 >&2"
     exit 1
 fi
-echo $version
+echo version=$version > /tmp/image-version-$1.txt
+echo "Found version for $1: $version"

65ec7440d9a2ffea44167ba36533b76a8461d661
antarcticrainforest <martin.bergemann@posteo.org>
2025-04-30 00:13:12 +0200

    Drop gosu.

--- docker-scripts/entrypoint.sh
+++ docker-scripts/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-APP_UID=${APP_UID:-0}
-APP_GID=${APP_GID:-0}
-
 export DATA_DIR=/data/db
 export LOG_DIR=/data/logs
 export CONFIG_DIR=/data/config
@@ -11,5 +8,4 @@ export BACKUP_DIR=/backup
 export PATH=/opt/conda/bin:$PATH
 export USER=$(whoami)
 
-mkdir -p $DATA_DIR $LOG_DIR $CONFIG_DIR $BACKUP_DIR || true
 exec "$@"

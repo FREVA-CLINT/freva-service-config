38b3d8750218de042ec5f123380a31661fab263e
antarcticrainforest <martin.bergemann@posteo.org>
2025-05-23 12:51:50 +0200

    Add debug flag.

--- docker-scripts/entrypoint.sh
+++ docker-scripts/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
+if [ "$DEBUG" = "true" ]; then
+    set -x
+fi
 
+export DEBUG
 export DATA_DIR=/data/db
 export LOG_DIR=/data/logs
 export CONFIG_DIR=/data/config
 export BACKUP_DIR=/backup
 export PATH=/opt/conda/bin:$PATH
-export USER=$(whoami)
+export USER=$(whoami) 2> /dev/null || $(id -u)
 mkdir -p $DATA_DIR $LOG_DIR $CONFIG_DIR $BACKUP_DIR
 exec "$@"
